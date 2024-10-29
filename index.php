c89893641eb8a8b550d2b71c1272e185d8e36be6
mirirowwwssss0 <annmisoles2003@gmail.com>
2024-10-29 16:48:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ https://templatemo.com/tm-562-space-dynamic
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
             <ul class="nav">
-              <li class="scroll-to-section"><a href="#top" class="active">home</a></li>
+              <li class="scroll-to-section"><a href="#top" class="active">home basa</a></li>
               <li class="scroll-to-section"><a href="#about">About Us</a></li>
               <li class="scroll-to-section"><a href="#services">Services</a></li>
               <li class="scroll-to-section"><a href="#portfolio">Portfolio</a></li>
